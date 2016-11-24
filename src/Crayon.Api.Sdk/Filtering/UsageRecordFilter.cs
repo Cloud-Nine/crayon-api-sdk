e36e515c955515bf076ef30e4a598c555c4593ad
Marcus Grönblad <marcus.gronblad@gmail.com>
2016-11-24 16:10:42 +0100

    Removed page and pagesize from UsageRecordFilter

--- src/Crayon.Api.Sdk/Filtering/UsageRecordFilter.cs
+++ src/Crayon.Api.Sdk/Filtering/UsageRecordFilter.cs
@@ -14,10 +14,6 @@ namespace Crayon.Api.Sdk.Filtering
 
         public DateTimeOffset To { get; set; }
 
-        public int Page { get; set; } = 1;
-
-        public int PageSize { get; set; } = 50;
-
         public string Search { get; set; }
         
         public string ToQueryString()
