86c2b7049d3a5ba762fc1b4e7bbcab520087fd52
Marcus Grönblad <marcus.gronblad@gmail.com>
2017-02-10 13:12:41 +0100

    Made UsageRecordFilter look like UsageRecordGroupedFilter

--- src/Crayon.Api.Sdk/Filtering/UsageRecordGroupedFilter.cs
+++ src/Crayon.Api.Sdk/Filtering/UsageRecordGroupedFilter.cs
@@ -9,7 +9,7 @@ namespace Crayon.Api.Sdk.Filtering
         public int SubscriptionId { get; set; }
         public int ProductFamilyId { get; set; }
         public int PublisherId { get; set; }
-        public int CustomerTenantId { get; set; }
+        public int CloudAccountId { get; set; }
         public DateTimeOffset? From { get; set; }
         public DateTimeOffset? To { get; set; }
         public int Page { get; set; } = 1;
